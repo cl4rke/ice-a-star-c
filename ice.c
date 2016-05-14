b42ab9965723bcf73e496aec2b7f5acc2197a15d
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2016-05-14 08:30:42 +0800

    Use variables

--- ice.c
+++ ice.c
@@ -9,11 +9,11 @@ int main() {
     int width = 5;
     int height = 6;
 
-    Maze* maze = read_maze(5, 6, file_source);
+    Maze* maze = read_maze(width, height, file_source);
 
     fclose(file_source);
 
     printf("Printing maze...\n");
-    print_maze(5, 6, maze);
+    print_maze(width, height, maze);
 }
 
